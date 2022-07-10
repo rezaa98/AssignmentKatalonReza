c186f685300d5c88008270e4cbfa511ac1a6d0ab
rezaa98 <reza.yusuf98@gmail.com>
2022-07-10 15:44:23 +0700

    Update Objek for Mobile Testing

---
+++ Assignment 3 Mobile Testing/Assignment3MobileTesting/Object Repository/Checkout/CEK ITEMS ORDER.rs
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>CEK ITEMS ORDER</name>
+   <tag></tag>
+   <elementGuidId>52d1a08e-e8b9-400f-8030-ee66038f2170</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>android.widget.EditText</value>
+      <webElementGuid>6ac2eb9d-e0d5-4129-a26a-10b06de1b696</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>index</name>
+      <type>Main</type>
+      <value>0</value>
+      <webElementGuid>89be67ae-e949-426f-81b9-31d1474f0028</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>2 Samsung Galaxy S10 - Black 1.798 USD,
+1 Animal Jumpsuit for Newborn Baby 10 USD,
+1 Nike Magista Obra II Club TF 42 USD,
+1 Hybrid Stroller Cabi Sport Rosse Gold  380 USD,
+
+Order : 2.230 USD
+Tax : 10 % : 223 USD
+Total : 2.453 USD</value>
+      <webElementGuid>16eed226-441b-45d3-8514-c35604cb7124</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>resource-id</name>
+      <type>Main</type>
+      <value>com.solodroid.solomerce:id/edt_order_list</value>
+      <webElementGuid>3d9a3cad-7ccc-4e53-895e-5101addc29ed</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>package</name>
+      <type>Main</type>
+      <value>com.solodroid.solomerce</value>
+      <webElementGuid>8c314a90-97aa-46d4-8d5d-6c9ef182ca45</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>checkable</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>66a5e4fe-fb0c-405d-b855-9e11cc5faf56</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>checked</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>d1403852-863f-41a0-96c5-719d1e01cfb3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>clickable</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>1a42860a-95a3-4e0e-9032-a9d8cddba3bc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>enabled</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>2e814177-ea3f-48b7-8a09-eea0069bac53</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>focusable</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>1a5f7a45-a5be-4198-ae9d-97bbc7b6f9c3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>focused</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>9faa6031-ad7a-4ed7-8fff-363e5fc73a0b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>scrollable</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>bc2fcaf0-3f26-4a72-a017-896e1482beac</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>long-clickable</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>022e6498-da15-4340-8873-2bed63bda7aa</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>password</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>dc9d9229-c28f-4efc-bb94-d34a8a8d1396</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>selected</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>6bc49cf3-7874-4e09-9afd-65ae7c96b157</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>x</name>
+      <type>Main</type>
+      <value>45</value>
+      <webElementGuid>a6c76983-7242-45e8-95a5-597fc1d2c21d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>y</name>
+      <type>Main</type>
+      <value>1254</value>
+      <webElementGuid>61a4a828-f947-41a0-b8ea-a06aa4ff9633</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>width</name>
+      <type>Main</type>
+      <value>990</value>
+      <webElementGuid>f2a1c3f8-9504-496d-80d6-8974acac1e1a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>height</name>
+      <type>Main</type>
+      <value>456</value>
+      <webElementGuid>c4725702-6329-4aa5-bb3f-ef30fb2b264b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>bounds</name>
+      <type>Main</type>
+      <value>[45,1254][1035,1710]</value>
+      <webElementGuid>f23feb55-4677-4285-99b5-24231b2bb5bd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>displayed</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>d6620891-cd3d-4893-9ff0-a4a936810ad9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[6]/android.widget.FrameLayout[1]/android.widget.EditText[1]</value>
+      <webElementGuid>1404cafc-0a1b-46a8-9b13-115ae0deeebb</webElementGuid>
+   </webElementProperties>
+   <locator>//*[@class = 'android.widget.EditText' and (@text = '2 Samsung Galaxy S10 - Black 1.798 USD,
+1 Animal Jumpsuit for Newborn Baby 10 USD,
+1 Nike Magista Obra II Club TF 42 USD,
+1 Hybrid Stroller Cabi Sport Rosse Gold  380 USD,
+
+Order : 2.230 USD
+Tax : 10 % : 223 USD
+Total : 2.453 USD' or . = '2 Samsung Galaxy S10 - Black 1.798 USD,
+1 Animal Jumpsuit for Newborn Baby 10 USD,
+1 Nike Magista Obra II Club TF 42 USD,
+1 Hybrid Stroller Cabi Sport Rosse Gold  380 USD,
+
+Order : 2.230 USD
+Tax : 10 % : 223 USD
+Total : 2.453 USD') and @resource-id = 'com.solodroid.solomerce:id/edt_order_list']</locator>
+   <locatorStrategy>ATTRIBUTES</locatorStrategy>
+</MobileElementEntity>
