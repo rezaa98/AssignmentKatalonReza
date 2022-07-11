09331d8d3118b9fe0a45fdedf7c96d3d3beaf081
rezaa98 <reza.yusuf98@gmail.com>
2022-07-11 16:31:49 +0700

    Update Script1657443092773.groovy

--- Assignment 3 Mobile Testing/Assignment3MobileTesting/Scripts/Ecommerce-TC01/Script1657443092773.groovy
+++ Assignment 3 Mobile Testing/Assignment3MobileTesting/Scripts/Ecommerce-TC01/Script1657443092773.groovy
@@ -19,55 +19,87 @@ import org.openqa.selenium.Keys as Keys
 
 //String appFile = GlobalVariable.androidAPP
 //Mobile.startApplication(appFile, true)
-Mobile.startExistingApplication(GlobalVariable.bundleID)
-Mobile.waitForElementPresent(findTestObject('Object Repository/Cek Profile/Verify Screen - E-Commerce Android App'), GlobalVariable.timeOUT, FailureHandling.STOP_ON_FAILURE)
+WebUI.comment('Start Aplication Ecommerce Android')
 
-if (Mobile.verifyElementNotExist(findTestObject('Object Repository/Cek Profile/Verify Screen - E-Commerce Android App'), GlobalVariable.timeOUT, FailureHandling.OPTIONAL)) {
-	Mobile.tap(findTestObject('Object Repository/Checkout/Button Back'), GlobalVariable.timeOUT)
+Mobile.startExistingApplication(GlobalVariable.bundleID)
+
+Mobile.waitForElementPresent(findTestObject('Object Repository/Cek Profile/Verify Screen - E-Commerce Android App'), GlobalVariable.timeOUT, 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.comment('Run If statement')
+
+if (Mobile.verifyElementNotExist(findTestObject('Object Repository/Cek Profile/Verify Screen - E-Commerce Android App'), 
+    GlobalVariable.timeOUT, FailureHandling.OPTIONAL)) {
+    Mobile.tap(findTestObject('Object Repository/Checkout/Button Back'), GlobalVariable.timeOUT)
 }
 
 Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.ImageView'), GlobalVariable.timeOUT)
-Mobile.verifyElementVisible(findTestObject('Object Repository/Cek Profile/VERIF SCREEN - Profile'), GlobalVariable.timeOUT, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.verifyElementVisible(findTestObject('Object Repository/Cek Profile/VERIF SCREEN - Profile'), GlobalVariable.timeOUT, 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.comment('Do Looping')
 
 //untuk looping edit data sesuai dengan data yang ada di excel.
 for (def rowNum = 1; rowNum <= findTestData('Profile').getRowNumbers(); rowNum++) {
+    WebUI.comment('do If Statement in Looping')
+
     if (findTestData('Profile').getValue('ACTIVE', rowNum) == 'Y') {
-        
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.TextView - EDIT'), GlobalVariable.timeOUT)
-		Mobile.verifyElementVisible(findTestObject('Object Repository/Cek Profile/VERIF SCREEN - User Profile'), GlobalVariable.timeOUT, FailureHandling.STOP_ON_FAILURE)
+
+        Mobile.verifyElementVisible(findTestObject('Object Repository/Cek Profile/VERIF SCREEN - User Profile'), GlobalVariable.timeOUT, 
+            FailureHandling.STOP_ON_FAILURE)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.RelativeLayout'), GlobalVariable.timeOUT)
+
         Mobile.clearText(findTestObject('Object Repository/Cek Profile/android.widget.EditText - Your Name'), GlobalVariable.timeOUT)
-        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue('Nama', rowNum), GlobalVariable.timeOUT)
+
+        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue(
+                'Nama', rowNum), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.Button - OK'), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.RelativeLayout (1)'), GlobalVariable.timeOUT)
-        Mobile.clearText(findTestObject('Object Repository/Cek Profile/android.widget.EditText - your.emailgmail.com'), GlobalVariable.timeOUT)
-        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue('Email', rowNum), GlobalVariable.timeOUT)
+
+        Mobile.clearText(findTestObject('Object Repository/Cek Profile/android.widget.EditText - your.emailgmail.com'), 
+            GlobalVariable.timeOUT)
+
+        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue(
+                'Email', rowNum), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.Button - OK'), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.RelativeLayout (2)'), GlobalVariable.timeOUT)
+
         Mobile.clearText(findTestObject('Object Repository/Cek Profile/android.widget.EditText - 628123456789'), GlobalVariable.timeOUT)
-        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue('HP', rowNum), GlobalVariable.timeOUT)
+
+        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue(
+                'HP', rowNum), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.Button - OK'), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.RelativeLayout (3)'), GlobalVariable.timeOUT)
+
         Mobile.clearText(findTestObject('Object Repository/Cek Profile/android.widget.EditText - Your Address'), GlobalVariable.timeOUT)
-        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue('Alamat', rowNum), GlobalVariable.timeOUT)
+
+        Mobile.setText(findTestObject('Object Repository/Cek Profile/android.widget.EditText'), findTestData('Profile').getValue(
+                'Alamat', rowNum), GlobalVariable.timeOUT)
 
         Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.Button - OK'), GlobalVariable.timeOUT)
-		
-		Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.ImageButton'), GlobalVariable.timeOUT)
 
-        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - Nama'), findTestData('Profile').getValue('Nama', rowNum), FailureHandling.STOP_ON_FAILURE)
-        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - Email'), findTestData('Profile').getValue('Email', rowNum), FailureHandling.STOP_ON_FAILURE)
-        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - HP'), findTestData('Profile').getValue('HP', rowNum), FailureHandling.STOP_ON_FAILURE)
-        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - Alamat'), findTestData('Profile').getValue('Alamat',  rowNum), FailureHandling.STOP_ON_FAILURE)
+        Mobile.tap(findTestObject('Object Repository/Cek Profile/android.widget.ImageButton'), GlobalVariable.timeOUT)
 
+        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - Nama'), findTestData('Profile').getValue('Nama', 
+                rowNum), FailureHandling.STOP_ON_FAILURE)
+
+        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - Email'), findTestData('Profile').getValue('Email', 
+                rowNum), FailureHandling.STOP_ON_FAILURE)
+
+        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - HP'), findTestData('Profile').getValue('HP', rowNum), 
+            FailureHandling.STOP_ON_FAILURE)
+
+        Mobile.verifyElementText(findTestObject('Cek Profile/TextView - Alamat'), findTestData('Profile').getValue('Alamat', 
+                rowNum), FailureHandling.STOP_ON_FAILURE)
     }
 }
 
-//Mobile.closeApplication()
