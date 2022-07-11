4eb0d19a0dfe621d450115906d3c93b3e586c026
rezaa98 <reza.yusuf98@gmail.com>
2022-07-11 16:26:44 +0700

    update again assignment 3

---
+++ Assignment 3 Mobile Testing/Assignment3MobileTesting/Test Suites/Test Suite/Test Suite Checkout.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Cek fungsi Checkout</description>
+   <name>Test Suite Checkout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>44331d9c-b978-42c8-beb6-badbffad505c</testSuiteGuid>
+   <testCaseLink>
+      <guid>db8424b7-cf4d-4d10-a34f-b2da2659a7a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ecommerce-TC07</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7d395d6-d1b3-4409-88cb-504d41518e27</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ecommerce-TC08</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
